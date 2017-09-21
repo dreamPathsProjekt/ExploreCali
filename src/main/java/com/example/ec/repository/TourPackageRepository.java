3df834956fe0ec9c99643ab4376c35bec4cf4582
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2017-09-21 10:30:53 +0300

    Enable Paging And Sorting Repositories

--- src/main/java/com/example/ec/repository/TourPackageRepository.java
+++ src/main/java/com/example/ec/repository/TourPackageRepository.java
@@ -3,11 +3,13 @@ package com.example.ec.repository;
 
 import com.example.ec.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  *
  * @author i.dritsas
  */
-public interface TourPackageRepository extends CrudRepository<TourPackage, String>{
-    public TourPackage findByName(String name);
+public interface TourPackageRepository extends PagingAndSortingRepository<TourPackage, String>{
+    public TourPackage findByName(@Param("name") String name);
 }
--- src/main/java/com/example/ec/repository/TourRepository.java
+++ src/main/java/com/example/ec/repository/TourRepository.java
@@ -2,12 +2,17 @@
 package com.example.ec.repository;
 
 import com.example.ec.domain.Tour;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  *
  * @author i.dritsas
  */
-public interface TourRepository extends CrudRepository<Tour, Integer>{
-    
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer>{
+    public Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);  //changes to enable paging and sorting |from Crud->PagingAndSorting.
 }
