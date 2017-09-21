486fc1e991b6982582e25a3d63dd0896a9e8c2a5
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2017-09-21 10:39:20 +0300

    Don't Expose Edit/Delete Resources & Rename TourPackages Uri Endpoint

--- src/main/java/com/example/ec/repository/TourPackageRepository.java
+++ src/main/java/com/example/ec/repository/TourPackageRepository.java
@@ -5,11 +5,40 @@ import com.example.ec.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  *
  * @author i.dritsas
  */
-public interface TourPackageRepository extends PagingAndSortingRepository<TourPackage, String>{
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages") //The Resource is now renamed as /packages.
+public interface TourPackageRepository extends CrudRepository<TourPackage, String>{
     public TourPackage findByName(@Param("name") String name);
+
+    @Override
+    @RestResource(exported = false)
+    public void deleteAll();
+
+    @Override
+    @RestResource(exported = false)
+    public void delete(Iterable<? extends TourPackage> itrbl);
+
+    @Override
+    @RestResource(exported = false)
+    public void delete(TourPackage t);
+
+    @Override
+    @RestResource(exported = false)
+    public void delete(String id);
+
+    @Override
+    @RestResource(exported = false)
+    public <S extends TourPackage> Iterable<S> save(Iterable<S> itrbl);
+
+    @Override
+    @RestResource(exported = false)
+    public <S extends TourPackage> S save(S s);
+    
+    
 }
--- src/main/java/com/example/ec/repository/TourRepository.java
+++ src/main/java/com/example/ec/repository/TourRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  *
@@ -15,4 +16,30 @@ import org.springframework.data.repository.query.Param;
  */
 public interface TourRepository extends PagingAndSortingRepository<Tour, Integer>{
     public Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);  //changes to enable paging and sorting |from Crud->PagingAndSorting.
+
+    @Override
+    @RestResource(exported = false) //prevent users of the Api to edit and delete Tours. Method level Annotation.
+    public <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    public void deleteAll();
+
+    @Override
+    @RestResource(exported = false)
+    public void delete(Iterable<? extends Tour> itrbl);
+
+    @Override
+    @RestResource(exported = false)
+    public void delete(Tour t);
+
+    @Override
+    @RestResource(exported = false)
+    public void delete(Integer id);
+
+    @Override
+    @RestResource(exported = false)
+    public <S extends Tour> Iterable<S> save(Iterable<S> itrbl);
+    
+    
 }
